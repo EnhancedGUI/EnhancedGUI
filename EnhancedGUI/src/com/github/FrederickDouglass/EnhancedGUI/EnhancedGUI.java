3f54c70c8c3fdc1f035b15229576a7465fcd0453
Douglas Rourke <drourke@clarku.edu>
2012-03-13 23:08:47 -0400

    Added a listener and began Javadoc

--- EnhancedGUI/src/com/github/FrederickDouglass/EnhancedGUI/EnhancedGUI.java
+++ EnhancedGUI/src/com/github/FrederickDouglass/EnhancedGUI/EnhancedGUI.java
@@ -1,45 +1,76 @@
-//Sample Comment
-//Updated the file path for the API libraries
+/**Enhanced GUI will add additional functionality to the Minecraft GUI
+ * 
+ * @author Douglas Rourke
+ * @author Krzysztof Kotarba
+ * @version 0.0.1
+ */
 package com.github.FrederickDouglass.EnhancedGUI;
 
 import java.io.File;
 import java.util.logging.Logger;
 
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-//import com.gmail.nossr50.config.LoadProperties;
+
 
 public class EnhancedGUI extends JavaPlugin
 {
-	
-	public static String maindirectory = "plugins" + File.separator + "EnhancedGUI";
-	static File versionFile = new File (maindirectory + File.separator + "VERSION");
-		
+	public static String maindirectory = "plugins" + File.separator + "EnhancedGUI";	
 	public static final Logger log = Logger.getLogger("Minecraft");
+	private static EnhancedGUI instance;
+	private File file; //Will be used to set up config.yml
 	
-	//For the Jar File
-	public static File EnhancedGUI;
 
-	
-	//Sets the parameters for what is initialized at the start
+	/** When the plugin is enabled, it will:
+	 *  -create a new instance 
+	 *  -pull data from the config.yml file
+	 *  -Initialize a listener
+	 *  
+	 * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
+	 */
 	public void onEnable() 
 	{
 		log.info("EnhancedGUI has been enabled.");
-		PluginManager pm = getServer().getPluginManager();
+		instance = this;
+		file = new File(getDataFolder(), "config.yml");
 		
-		if (pm.getPlugin("Spout") != null)
-			System.out.println("Spout found! Initializing EnhancedGUI");
-		else
-			System.out.println("You must have Spout in order to use this plugin!");
+		getServer().getPluginManager().registerEvents(new EGListener(this),this);
+		
 	}
 	
 	
+	/**Allows the player to be sent a message in game
+	 * 
+	 */
+	public void send(Player player, String message, ChatColor color) 
+	{
+		player.sendMessage(color + message);
+	}
+	public void send(Player player, String message) 
+	{
+		send(player, message, ChatColor.AQUA);
+	}
 	
 	
+	/** Displays a message when the plugin is disabled
+	 * 
+	 * @see org.bukkit.plugin.java.JavaPlugin#onDisable()
+	 */
 	public void onDisable()
 	{
 		log.info("EnhancedGUI has been disabled");
+	}
+	
+	
+	/**Returns the instance of the mod
+	 * 
+	 * @return instance;
+	 */
+	public static final EnhancedGUI getInstance()
+	{
+		return instance;
 	}
 	
 
